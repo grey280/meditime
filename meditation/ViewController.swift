aa39c8a8963398cd01348d0e43282d7204a34509
Grey Patterson <greypatterson@gmail.com>
2018-07-05 13:24:01 -0700

    Initial Commit

---
+++ meditation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  meditation
+//
+//  Created by Grey Patterson on 2018-07-05.
+//  Copyright © 2018 Grey Patterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
