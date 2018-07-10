25e465b00e9358cd6ed7d89fbb4d455e274ea3a8
Grey Patterson <greypatterson@gmail.com>
2018-07-10 15:40:37 -0700

    Set up the `time` `didChange` stuff, including using a `DateComponentsFormatter` to get a nice way to display everything.

--- meditation/MainViewController.swift
+++ meditation/MainViewController.swift
@@ -19,13 +19,23 @@ class MainViewController: UIViewController {
     /// The amount of time, in seconds, for the timer to run
     var time = 0{
         didSet{
+            // Show which mode we're in
             if time == 0{
-                
+                timerMode.textColor = constants.colors.mindful ?? UIColor.clear
+                stopwatchMode.textColor = constants.colors.darker ?? UIColor.black
+            }else{
+                timerMode.textColor = constants.colors.darker ?? UIColor.black
+                stopwatchMode.textColor = constants.colors.mindful ?? UIColor.clear
             }
+            // Format the amount of seconds and write it to the screen
+            clockDisplay.text = formatter.string(from: TimeInterval(time))
         }
     }
     /// Locally-accessible connection to `UserDefaults.standard`
     let userDefaults = UserDefaults.standard
+    
+    /// Our date components formatter; configured during `viewDidLoad`, can then be used to get formatted strings in the way we want.
+    private let formatter = DateComponentsFormatter()
     
     // MARK: - Outlets
     
@@ -84,6 +94,10 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         // Hide the 'save to health' button; it'll be displayed at the end of a session if we need to ask for permission to save it.
         saveToHealthButton.isHidden = true
+        // Configure the date/time formatter, since it'll be used by setting the time, which is one of our next steps. Thanks go to [CrunchyBagel](https://crunchybagel.com/formatting-a-duration-with-nsdatecomponentsformatter/) for the how-to on this.
+        formatter.unitsStyle = .positional
+        formatter.allowedUnits = [ .minute, .second ]
+        formatter.zeroFormattingBehavior = [ .pad ]
         // Load the time from defaults; helpfully, we want 0 to be the default if we don't have something set
         time = userDefaults.integer(forKey: constants.timeKey)
     }
