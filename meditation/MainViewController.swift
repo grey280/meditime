538095150063738a68ecdcfb9b2e79762b0a66a0
Grey Patterson <greypatterson@gmail.com>
2018-07-09 17:10:24 -0700

    Outlet connections

--- meditation/MainViewController.swift
+++ meditation/MainViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    @IBOutlet weak var saveToHealthButton: UIButton!
+    
+    @IBOutlet weak var clockDisplay: UILabel!
+    @IBOutlet weak var timeDisplay: UIView!
+    
+    @IBOutlet weak var timerMode: UILabel!
+    @IBOutlet weak var stopwatchMode: UILabel!
+    
+    
+    
+    @IBAction func saveToHealth(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
