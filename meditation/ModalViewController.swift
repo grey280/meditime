762a0319a2fc5ae35e6eac111f8d3134f7aecda1
Grey Patterson <greypatterson@gmail.com>
2018-07-13 16:33:43 -0700

    Ability to dismiss the modal

---
+++ meditation/ModalViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ModalViewController.swift
+//  meditation
+//
+//  Created by Grey Patterson on 2018-07-13.
+//  Copyright © 2018 Grey Patterson. All rights reserved.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+    
+    @IBAction func dismissVC(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
