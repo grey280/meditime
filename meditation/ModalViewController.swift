11b4aac68d91f6cb00bbdcb4cdae32321c745787
Grey Patterson <greypatterson@gmail.com>
2018-07-15 13:54:03 -0700

    debugging

--- meditation/ModalViewController.swift
+++ meditation/ModalViewController.swift
@@ -13,4 +13,9 @@ class ModalViewController: UIViewController {
     @IBAction func dismissVC(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("Appeared")
+    }
 }
