6366f1393c73ee82858e725fc57c98d5e8df0fd2
grey280 <greypatterson@gmail.com>
2018-11-15 19:17:16 -0800

    Start creating a Health class

---
+++ meditation/Health.swift
@@ -0,0 +1,15 @@
+//
+//  Health.swift
+//  meditation
+//
+//  Created by Grey Patterson on 11/15/18.
+//  Copyright © 2018 Grey Patterson. All rights reserved.
+//
+
+import Foundation
+import HealthKit
+
+/// Handles all the interactions with HealthKit
+class Health{
+    
+}
